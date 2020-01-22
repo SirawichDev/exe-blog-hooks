a776e65ee509d53eec9fbc40e596a1d8d0844207
SirawichDev <sirawit0676@gmail.com>
2020-01-23 05:35:24 +0700

    put fotter

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Layout} from 'antd'
+
+const {Footer} = Layout
+const Footers = () => {
+
+    return (
+        <Footer>Footer</Footer>
+    )
+}
+
+export default Footers
