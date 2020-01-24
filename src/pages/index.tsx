eac094addb244f31a84eeb034b1ebabd57138544
SirawichDev <sirawit0676@gmail.com>
2020-01-25 00:21:15 +0700

    add some component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,15 @@ import Layout from "../components/Layout/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import '../sass/index.scss'
+import SubList from "../components/SubList"
+import { Row, Col } from "antd"
 const IndexPage = () => (
   <Layout>
+    <Row>
+      <Col span={24}>
     <SEO title="Home" />
+    <SubList/>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -15,6 +21,8 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/blog/">Go to page 2</Link>
+    </Col>
+    </Row>
   </Layout>
 )
 
