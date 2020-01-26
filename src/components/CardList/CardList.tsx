abeb793310c4aed38f869b1ed428dd10a29ee758
SirawichDev <sirawit0676@gmail.com>
2020-01-26 11:56:27 +0700

    card List seperate 2 part

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -2,16 +2,19 @@ import React from "react"
 import { Row, Col } from "antd"
 import CardContent from "./CartContent"
 import CardThumb from "./CardTrumbnail"
-import '../../sass/cardList.scss'
+import "../../sass/cardList.scss"
 const CardList = () => {
   return (
     <Row type="flex" justify="space-around">
-      <Col span={20}>
-        <CardContent />
-      </Col>
-      <Col span={10}>
-        <CardThumb />
-      </Col>
+      <div className="exe-blog-item">
+        {" "}
+        <Col span={12}>
+          <CardContent />
+        </Col>
+        <Col span={10}>
+          <CardThumb />
+        </Col>
+      </div>
     </Row>
   )
 }
