8c64c41d62c5144cb8c3af6224de81ffcf6d6c95
SirawichDev <sirawit0676@gmail.com>
2020-01-26 11:38:14 +0700

    x

---
+++ src/components/CardList/CartContent.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { Icon, Row, Col } from "antd"
+import ReadMoreArrow from "../../images/custom-icons/readmore-arrow.png"
+import ReadMoreArrowBack from "../../images/custom-icons/readmore-arrow-black.png"
+const CardContent = () => {
+  return (
+    <>
+      <Row>
+        <Col>
+          <div className="blog-item-content">
+            <Col span={24}>
+              <h6 className="blog-item-date">
+                <span className="date">10</span> th{" "}
+                <span className="month">March</span>
+              </h6>
+            </Col>
+            <Col span={24}>
+              <h4 className="blog-item-title">
+                <a href="blog-details.html">
+                  How To Install Flutter
+                </a>
+              </h4>
+            </Col>
+            <div className="blog-item-share">
+              <Col span={24}>
+                <button className="blog-item-share-toggle">
+                  <Icon type="share-alt" style={{fontSize: '20px', marginRight: '1px'}} /> <p>Shared</p>
+                </button>
+              </Col>
+            </div>
+            <Col span={24}>
+              <a href="blog-details.html" className="exe-readmore">
+                Read More
+                <span>
+                  <img src={ReadMoreArrowBack} alt="Readmore Arrow" />
+                  <img src={ReadMoreArrow} alt="Readmore Arrow" />
+                </span>
+              </a>
+            </Col>
+          </div>
+        </Col>
+      </Row>
+    </>
+  )
+}
+export default CardContent
---
+++ src/components/CardList/index.ts
@@ -0,0 +1,4 @@
+import CardList from './CardList'
+
+
+export default CardList
