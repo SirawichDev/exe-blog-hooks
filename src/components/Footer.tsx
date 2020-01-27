9f939c476501e098ac59fcb1530faa2403ca7c70
SirawichDev <sirawit0676@gmail.com>
2020-01-27 18:14:19 +0700

    useStaticQuery

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import {Layout} from 'antd'
+import {Layout, Row} from 'antd'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const siteQuery = graphql`{
+
+    site {
+        siteMetadata{
+            author
+            title
+        }
+    }
+}
+`
 
 const {Footer} = Layout
 const Footers = () => {
+    const {site: {siteMetadata},} = useStaticQuery(siteQuery)
+
+    console.log('ss',siteMetadata)
 
     return (
-        <Footer>Footer</Footer>
+    <Footer>
+        <Row type="flex" justify="end">
+        Created By {siteMetadata.author}</Row></Footer>
     )
 }
 
