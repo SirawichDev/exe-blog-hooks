7265cb8c1f858d64c35a227802700662492f7c3c
SirawichDev <sirawit0676@gmail.com>
2020-01-28 23:44:08 +0700

    query logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,32 @@
 import React, { FC } from "react"
 import "../sass/navbar.scss"
 import { Layout, Menu, Icon, Row, Col, Breadcrumb, Typography } from "antd"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import HeaderText from "./HeaderText"
 const { Header, Content } = Layout
 const { Text } = Typography
 
+const logoQuery = graphql`
+  query{
+    file(relativePath: {eq:"exe-blog-x.png"}) {
+      publicURL
+    }
+  }
+`
 interface Props {
   location?: any
 }
 const NavBar: FC<Props> = ({ location }) => {
+  const {
+    file: { publicURL },
+  } = useStaticQuery(logoQuery)
+  console.log("relativePath", publicURL)
+  const logoFixed =publicURL;
   return (
     <Row type="flex" justify="end">
       <Header className="header" style={{ backgroundColor: "transparent" }}>
-        <Col span={12}>
-          <Text>LOGO</Text>
+        <Col span={12} offset={1}>
+          <a><img style={{width: '150px', height:'140px'}} src={`${logoFixed}`}/></a>
         </Col>
         <Row type="flex" justify="end">
           <Col sm={23} xs={10} lg={18}>
@@ -39,7 +51,7 @@ const NavBar: FC<Props> = ({ location }) => {
               </Menu.Item>
               <Menu.Item>
                 {" "}
-                <Link to="/lifeStyle" state={{ pathName: "life_style" }}>
+                <Link to="/lifestyle" state={{ pathName: "life_style" }}>
                   <Icon type="sync" spin />
                   Lift Style
                 </Link>
