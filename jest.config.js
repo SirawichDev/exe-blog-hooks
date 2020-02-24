ae72d6b105b4a5967a8215d048e99722247d1e33
SirawichDev <sirawit0676@gmail.com>
2020-02-24 15:17:52 +0700

    isolate fase and remove another thing

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,4 @@ module.exports = {
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transformIgnorePatterns: [
-      "node_modules/(?!(react-native"
-        + "|react-navigation-tabs"
-        + "|react-native-splash-screen"
-        + "|react-native-screens"
-        + "|react-native-reanimated"
-      + ")/)",
-    ],
   }
