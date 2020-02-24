3373de26153f7b9b0677638b20c2091975ec2009
SirawichDev <sirawit0676@gmail.com>
2020-02-24 20:18:50 +0700

    fix codecov

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
     transform: {
       '^.+\\.tsx?$': 'ts-jest'
     },
+    collectCoverage: true,
+    coverageDirectory: './coverage/',
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
