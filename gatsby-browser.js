68db6dc20b9060931c9f02eb34f86df07ba00bc4
SirawichDev <sirawit0676@gmail.com>
2020-02-29 00:22:27 +0700

    set the shaed remmark theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,2 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+require("./src/assets/prism-theme/shade-of-purple.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
