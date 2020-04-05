987490386b1f3ed2186dc973963b9c2731b08fd1
SirawichDev <sirawit0676@gmail.com>
2020-04-05 22:22:02 +0700

    diagnostic set

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,17 @@ module.exports = {
     coverageDirectory: './coverage/',
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     testEnvironment: 'node',
+    setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    collectCoverage: true
+    snapshotSerializers: ["enzyme-to-json/serializer"],
+    globals: {
+        // we must specify a custom tsconfig for tests because we need the typescript transform
+        // to transform jsx into js rather than leaving it jsx such as the next build requires. you
+        // can see this setting in tsconfig.jest.json -> "jsx": "react"
+        "ts-jest": {
+            "tsConfig": "<rootDir>/tsconfig.jest.json",
+            diagnostics: false,
+        }
+    }
+
   }
