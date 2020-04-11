f310fe1657a342db7faa1936eca79a76232053dd
SirawichDev <sirawit0676@gmail.com>
2020-04-11 23:22:09 +0700

    do paginate

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,13 +12,28 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-  console.log("backend", data)
   data.devs.edges.forEach(({ node }) => {
     createPage({
       path: `blogs/${node.slug}`,
       component: path.resolve("./src/template/dev-template.tsx"),
       context: {
         slug: node.slug,
+      },
+    })
+  })
+  const blogs = data.devs.edges
+  const postPerPage = 4
+  const numPage = Math.ceil(blogs.length / postPerPage)
+
+   Array.from({ length: numPage }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/dev-blogs` : `/dev-blogs/${i + 1}`,
+      component: path.resolve("./src/template/blog-list-template.tsx"),
+      context: {
+        limit: postPerPage,
+        skip: i * postPerPage,
+        numPage,
+        currentPage: i + 1,
       },
     })
   })
